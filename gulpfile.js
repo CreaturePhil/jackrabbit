15f87d660c12593e235ba9258a493e3cca5d29b9
Phil <birkal@outlook.com>
2014-10-25 16:11:09 -0700

    Add preview in posts

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglifyjs');
 
 gulp.task('default', function() {
   gulp.src(['public/css/bootstrap.min.css', 'public/css/font-awesome.min.css','public/css/custom.css'])
     .pipe(concatCss('main.min.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('js', function() {
+  gulp.src(['public/js/bootstrap.min.js', 'public/js/marked.js'])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'))
 });
 
 gulp.task('watch', function() {
