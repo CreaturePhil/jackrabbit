1f6df9092b5b6d1c9d050bab3d40e1243ec8d426
Phil <birkal@outlook.com>
2014-10-26 01:23:02 -0700

    Updated stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,26 @@ var concatCss = require('gulp-concat-css');
 var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglifyjs');
+var jpegtran = require('imagemin-jpegtran');
 
 gulp.task('default', function() {
-  gulp.src(['public/css/bootstrap.min.css', 'public/css/font-awesome.min.css','public/css/custom.css'])
+  gulp.src(['public/css/bootstrap.min.css', 'public/css/font-awesome.min.css','public/css/custom.css', 'public/css/animate.css'])
     .pipe(concatCss('main.min.css'))
     .pipe(minifyCss())
     .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('js', function() {
-  gulp.src(['public/js/bootstrap.min.js', 'public/js/marked.js'])
+  gulp.src(['public/js/bootstrap.min.js', 'public/js/marked.js', 'public/js/wow.js'])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('img', function () {
+  gulp.src('public/*.jpg')
+    .pipe(jpegtran({ progressive: true })())
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('watch', function() {
